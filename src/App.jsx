d34e2c47f29dd49082fdc25f0f137cf261dc9704
bayansuluu <bayanamanapbaeva@gmail.com>
2025-01-31 18:47:02 +0600

    add why

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Read from './components/Read/Read'
 import Header from './components/Header/Header'
 import Trust from './components/Trust/Trust'
 import Main from './components/Main/Main'
+import Why from './components/Why/Why'
 
 
 
@@ -15,6 +16,7 @@ function App() {
     <Read/>
     <Trust/>
     <Category/>
+    <Why/>
     </>
   )
 }
